aae25047a26c962f696460e4c75f60e392deedd3
Antonio Flores <johndoe@johns-mbp.home>
2021-06-08 21:43:48 -0400

    Set up searchBar and changed initial recipeModel that populates tableView to FilterRecipeList, added keyboard handling and tableView empty state message in new extension file.

--- Recipe App/MVC/Controllers/RecipeListViewController.swift
+++ Recipe App/MVC/Controllers/RecipeListViewController.swift
@@ -8,17 +8,26 @@
 import UIKit
 
 class RecipeListViewController: UIViewController {
-
+    
     @IBOutlet weak var recipeListTableView: UITableView!
     @IBOutlet weak var recipeListSearchBar: UISearchBar!
     
     let recipeModel = Recipe.getRecipes()
     
+    var filteredRecipeList = [Recipe](){
+        didSet {
+            recipeListTableView.reloadData()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         delegateDatasourceSetup()
+        filteredRecipeList = recipeModel
+        self.recipeListTableView.keyboardDismissMode = .onDrag
     }
-
+    
     
     func delegateDatasourceSetup(){
         recipeListTableView.dataSource = self
@@ -26,28 +35,45 @@ class RecipeListViewController: UIViewController {
         recipeListSearchBar.delegate = self
     }
     
-
+    
 }
+
 
 extension RecipeListViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return recipeModel.count
+        return filteredRecipeList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let recipeListCell = recipeListTableView.dequeueReusableCell(withIdentifier: "recipeListCell", for: indexPath) as? RecipeListTableViewCell else {return UITableViewCell() }
         
-        recipeListCell.recipeImage.image = recipeModel[indexPath.row].image
-        recipeListCell.recipeName.text = recipeModel[indexPath.row].name
-        recipeListCell.recipeRating.text = "Rating: \(recipeModel[indexPath.row].rating)"
+        recipeListCell.recipeImage.image = filteredRecipeList[indexPath.row].image
+        recipeListCell.recipeName.text = filteredRecipeList[indexPath.row].name
+        recipeListCell.recipeRating.text = "Rating: \(filteredRecipeList[indexPath.row].rating)"
+        
         return recipeListCell
     }
+    
 }
 
 extension RecipeListViewController: UISearchBarDelegate {
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty{
+            filteredRecipeList = recipeModel
+        }  else {
+            filteredRecipeList = recipeModel.filter({$0.name.contains(searchText)})
+            recipeListTableView.restore()
+        }
+        if  filteredRecipeList.isEmpty {
+            recipeListTableView.setEmptyMessage("No Results Found")
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        recipeListSearchBar.resignFirstResponder()
+    }
 }
-
 
